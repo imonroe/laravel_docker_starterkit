f53ed0518eb817edaf1129c3b232d21d8cc84768
Ian Monroe <ian@ianmonroe.com>
2019-08-25 14:31:18 -0600

    wip

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 
-# echo Waiting on the database to be ready.
-# /wait-for-it.sh ${COMPOSE_PROJECT_NAME}_mysql:3306
-# echo Database is responding.
-
 cd /var/www
+
+if [ ! -f /var/www/.env ]; then
+FIRST_RUN=true
+else
+FIRST_RUN=false
+fi
+
+if [ $FIRST_RUN = true]; then
+echo Setting up the nginx host file.
+sed -i "s/__CONTAINER_NAME__/$PROJECT_WEBSERVER/g" /etc/nginx/conf.d/app.conf
+echo Host file updated.
+fi
 
 echo Installing Composer dependencies.
 composer install
 echo Completed Composer install.
 
 echo Checking out the environment.
-if [ ! -f /var/www/.env ]; then
+if [ $FIRST_RUN = true]; then
 # we are running this app for the first time.
 echo This is a first run, so we will create the environment file.
 cp /var/www/.env.example /var/www/.env
 cd /var/www
 php artisan key:generate
-echo Setting up the nginx host file.
-sed -i "s/__CONTAINER_NAME__/$PROJECT_WEBSERVER/g" /etc/nginx/conf.d/app.conf
-echo Host file updated.
 fi
 echo Finished checking out the environment.
 
